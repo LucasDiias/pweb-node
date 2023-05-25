eac856f1427c115ee24c4420181e62781bcc1e0b
Lucas Dias <maykonlucasdias@gmail.com>
2023-05-24 23:00:37 -0300

    finished av3

--- av3/server.js
+++ av3/server.js
@@ -5,23 +5,26 @@ const port = 3000;
 
 const alunos = [
   {
-    matricula: "2021319327",
+    id: 1,
     nome: "Lucas Dias",
     dataNasc: "2005-04-16",
+    matricula: "2021319327",
     status: "Ativo",
     email: "mlfd1@aluno.ifal.edu.br",
   },
   {
-    matricula: "2021321147",
+    id: 2,
     nome: "Sofia Rizzotto",
     dataNasc: "2006-01-07",
+    matricula: "2021321147",
     status: "Ativo",
     email: "spr2@aluno.ifal.edu.br",
   },
   {
-    matricula: "2021321148",
+    id: 3,
     nome: "João Pedro",
     dataNasc: "2006-11-26",
+    matricula: "2021321148",
     status: "Ativo",
     email: "jpam1@aluno.ifal.edu.br",
   },
@@ -29,32 +32,56 @@ const alunos = [
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home", { alunos: alunos });
 });
 
-app.get("/aluno", (req, res) => {
-  let options = {
-    title: "Novo Aluno",
-    aluno: null,
-  };
-  if (req.query.matricula) {
-    const { matricula } = req.query;
-    options = {
-      title: "Editar Aluno",
-      aluno: alunos.find((aluno) => aluno.matricula === matricula),
-    };
+app.get("/aluno/:id?", (req, res) => {
+  const { id } = req.params;
+  const aluno = alunos.find((aluno) => aluno.id === Number(id));
+
+  if (aluno) {
+    res.render("aluno", { title: "Editar Aluno", aluno: aluno });
+  } else {
+    res.render("aluno", { title: "Cadastrar Aluno", aluno: null });
   }
-  res.render("aluno", options);
 });
 
-app.delete("/alunos", (req, res) => {
-  const { matricula } = req.body;
-  const index = alunos.findIndex((aluno) => aluno.matricula === matricula);
+app.delete("/alunos/:id", (req, res) => {
+  const { id } = req.params;
+  const index = alunos.findIndex((aluno) => aluno.id === Number(id));
 
-  alunos.splice(index, 1);
+  if (index !== -1) {
+    alunos.splice(index, 1);
+  }
+});
+
+app.post("/alunos/:id?", (req, res) => {
+  const { id } = req.params;
+  const { nome, dataNasc, matricula, status, email } = req.body;
+
+  if (id) {
+    const index = alunos.findIndex((aluno) => aluno.id === Number(id));
+    if (index !== -1) {
+      alunos[index] = {
+        id: Number(id),
+        nome,
+        dataNasc,
+        matricula,
+        status,
+        email,
+      };
+    }
+  } else {
+    const newId = alunos.length > 0 ? alunos[alunos.length - 1].id + 1 : 1;
+    alunos.push({ id: newId, nome, dataNasc, matricula, status, email });
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
